ee4588de6ec78635fd2dbb00f87b6eb8efb059f1
Abraham <1584805@senati.pe>
2025-04-26 09:21:44 -0500

    Enviando Actualizacion

--- java3.js
+++ java3.js
@@ -7,10 +7,30 @@ const params = new URLSearchParams(window.location.search);
       descripcion: "Battle Royale de Epic Games.",
       imagen: "img2/Fortnite _ Xbox.jpg"
     },
-    cod: {
-        nombre: "Call of Duty",
+    CallofDutyWarzone: {
+        nombre: "Call of Duty Warzone",
         descripcion: "Juego de disparos en primera persona.",
-        imagen: "cod.jpg"
+        imagen: "img2/Call of Duty Warzone System Requirements PC (2021).jpg"
+    },
+    pubg: {
+        nombre: "Pubg Battlegrounds",
+        descripcion: "Juego de disparos en primera persona.",
+        imagen: "image/pubg.jpg"
+    },
+    Battlefield1 :{
+        nombre: "Battlefield 1",
+        descripcion: "Juego de disparos",
+        imagen: "img2/Aucune bataille ne se ressemble dans Battlefield 1.jpg"
+    },
+    counterstrike2 :{
+        nombre: "Counter Strike 2",
+        descripcion: "Juego de disparos en primera persona",
+        imagen: "img2/counter strike 2.jpg"
+    },
+    ArmaIII : {
+        nombre: "Arma III",
+        descripcion: "Juego de disparos en primera persona",
+        imagen: "img2/Arma III.jpg"
     }
   };
 
